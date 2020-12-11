cf3fe4c02e4673f0f552f3c0e0f89ac4cfd20611
racross1 <racross1@gmail.com>
2020-12-10 16:54:59 -0800

    done

--- app.rb
+++ app.rb
@@ -2,5 +2,29 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+get '/' do
+    "I just don't want an error message here"
+end 
+
+get '/newteam' do
+    erb :newteam
+end 
+
+
+
+post '/team' do
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @pf = params[:pf]
+    @sf = params[:sf]
+    @c = params[:c]
+
+
+    erb :team
+end 
+
+
 
 end
